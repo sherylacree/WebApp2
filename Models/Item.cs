fbcf9c2f2c88b31f13f58c4b00096767a5b8c35a
sheryl acree <sacree@tql.com>
2024-06-27 11:49:27 -0400

    AddedClassCustomerItem

---
+++ Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp2.Models;
+
+public class Item {
+    public int Id { get; set; }
+    [StringLength(30)]
+    public string Description { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(9,2)")]
+    public decimal Price { get; set; }  
+}
